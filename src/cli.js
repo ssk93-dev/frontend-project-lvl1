6c9d963af3869c0a3bbdb7572471c306a37a39d8
Sergei Kalikin <sskalikin@gmail.com>
2021-05-27 23:12:27 +0500

    add: git actions

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
 
-export const greeting = () => {
-  const userName = readlineSync.question("May I have your name? ");
-  console.log("Hi " + userName + "!");
+export default () => {
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hi ${userName}!`);
 };
